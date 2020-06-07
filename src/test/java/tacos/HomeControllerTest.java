38b6c8dcb21ec88051cd1ca067c2cbdfb606a759
antoniocassano84 <antonio.cassano984@gmail.com>
2020-06-07 09:34:41 +0100

    use junit4

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,30 +1,30 @@
 package tacos;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-@SpringBootTest
-@WebMvcTest
-class HomeControllerTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@Test
-	void testHomePage() throws Exception {
+	public void testHomePage() throws Exception {
 		mockMvc.perform(get("/")).andExpect(status().isOk())
 		                         .andExpect(view().name("home"))
-		                         .andExpect((ResultMatcher) content().string(containsString("Welcome to...")));
+		                         .andExpect(content().string(containsString("Welcome to...")));
 	}
 
 }
